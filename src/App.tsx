4986ab11f3997dea256c37d5f72747608e7480fa
jungwoow <yeriel.390@gmail.com>
2024-07-27 04:39:28 +0900

    + 은영님

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,17 @@ import MAIN_4 from "./components/main/main_4";
 import MAIN_3 from "./components/main/main_3";
 import MAIN_2 from "./components/main/main_2";
 import TestResult from "./components/q_2/test_result";
+import ResultPage1 from "pages/Propensity_analysis/ResultPage1";
+import ResultPage2 from "pages/Propensity_analysis/ResultPage2";
+import ResultPage3 from "pages/Propensity_analysis/ResultPage3";
+import ResultPage4 from "pages/Propensity_analysis/ResultPage4";
+import ResultPage5 from "pages/Propensity_analysis/ResultPage5";
+import ResultPage6 from "pages/Propensity_analysis/ResultPage6";
+import ResultPage7 from "pages/Propensity_analysis/ResultPage7";
+import ResultPage8 from "pages/Propensity_analysis/ResultPage8";
+import Propensity_analysis from "pages/Propensity_analysis/Propensity_analysis";
+import Profile from "pages/My_page/Profile";
+import ProfileUpload from "components/profileUpload";
 
 function App() {
   return (
@@ -46,6 +57,17 @@ function App() {
         <Route path="/main/main_4" element={<MAIN_4 />} />
         <Route path="/main/main_3" element={<MAIN_3 />} />
         <Route path="/main/main_2" element={<MAIN_2 />} />
+        <Route path="/analy" element={<Propensity_analysis />}></Route>
+        <Route path="/analy/resultpage1" element={<ResultPage1 />}></Route>
+        <Route path="/analy/resultpage2" element={<ResultPage2 />}></Route>
+        <Route path="/analy/resultpage3" element={<ResultPage3 />}></Route>
+        <Route path="/analy/resultpage4" element={<ResultPage4 />}></Route>
+        <Route path="/analy/resultpage5" element={<ResultPage5 />}></Route>
+        <Route path="/analy/resultpage6" element={<ResultPage6 />}></Route>
+        <Route path="/analy/resultpage7" element={<ResultPage7 />}></Route>
+        <Route path="/analy/resultpage8" element={<ResultPage8 />}></Route>
+        <Route path="/mypage/profile" element={<Profile />}></Route>
+        <Route path="/test" element={<ProfileUpload />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/resultFunc.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+
+export const useGotoRetest = () => {
+  const navigate = useNavigate();
+  return () => navigate('/analy');
+};
+
+export const useGotoSignUp = () => {
+  const navigate = useNavigate();
+  return () => navigate('/auth/sign-up');
+};
