95e64862c54aff865bf49f41bb70c5340d3d1e46
jungwoow <yeriel.390@gmail.com>
2024-07-27 05:12:02 +0900

    scraplist 수정

--- src/pages/ScrapPage/ScrapListPage.tsx
+++ src/pages/ScrapPage/ScrapListPage.tsx
@@ -3,11 +3,10 @@ import { useNavigate } from "react-router-dom";
 import ScrapList from "../../components/ScrapList";
 import { useScrap } from "../../components/ScrapContext";
 import Add from "components/scrapPoppup/ScrapFolderAdd";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "./ScrapListPage.css";
 
 const ScrapListPage: React.FC = () => {
-  const { scraps, createFolder, updateScrapOrder } = useScrap();
+  const { scraps, createFolder } = useScrap();
   const [newFolder, setNewFolder] = useState("");
   const [selectedFolder, setSelectedFolder] = useState("");
   const [showAdd, setShowAdd] = useState(false);
@@ -31,19 +30,6 @@ const ScrapListPage: React.FC = () => {
     setShowAdd(false);
   };
 
-  const onDragEnd = (result: any) => {
-    if (!result.destination) return;
-
-    const { source, destination } = result;
-    if (!selectedFolder) return;
-
-    const items = Array.from(scraps[selectedFolder].items);
-    const [movedItem] = items.splice(source.index, 1);
-    items.splice(destination.index, 0, movedItem);
-
-    updateScrapOrder(selectedFolder, items);
-  };
-
   return (
     <div style={{ display: "flex" }}>
       <div style={{ flexGrow: 1 }}>
@@ -65,29 +51,13 @@ const ScrapListPage: React.FC = () => {
         폴더 추가하기
       </button>
       {selectedFolder && (
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="scrap-list">
-            {(provided) => (
-              <div {...provided.droppableProps} ref={provided.innerRef}>
-                {scraps[selectedFolder].items.map((scrap, index) => (
-                  <Draggable key={scrap} draggableId={scrap} index={index}>
-                    {(provided) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        className="scrapItem"
-                      >
-                        {scrap}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
+        <div>
+          {scraps[selectedFolder].items.map((scrap, index) => (
+            <div key={index} className="scrapItem">
+              {scrap}
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
