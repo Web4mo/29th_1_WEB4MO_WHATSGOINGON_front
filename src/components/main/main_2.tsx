cb91cca96ae4dec8cd25fc2b7a8597ac8aec5e3e
hiimminjeong <jmjeong38@gmail.com>
2024-08-02 22:02:06 +0900

    메인화면 더미데이터, 스크랩 기능 연결(카테고리 기사 제외)

--- src/components/main/main_2.tsx
+++ src/components/main/main_2.tsx
@@ -1,11 +1,19 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import WGO from "../../assets/icons/whats_going_on.svg";
 import TITLE from "../../assets/icons/main_title.svg";
 import IMG from "../../assets/icons/img.jpg";
 import PENCIL from "../../assets/icons/pencil.svg";
 import Q10 from "../../assets/icons/q_1_0.svg";
+import Q11 from "../../assets/icons/q_1_1.svg";
 import Q12 from "../../assets/icons/q_1_2.svg";
+import Q13 from "../../assets/icons/q_1_3.svg";
+import Q14 from "../../assets/icons/q_1_4.svg";
+import Q15 from "../../assets/icons/q_1_5.svg";
+import Q16 from "../../assets/icons/q_1_6.svg";
+import Q17 from "../../assets/icons/q_1_7.svg";
+import SaveScrap from "components/saveScrap";
+import RedirectModal from "components/redirectModal";
 import "./main.css";
 
 const newsIcons = [
@@ -64,13 +72,111 @@ const topics = [
   { name: "데일리안", path: "https://www.dailian.co.kr/" },
 ];
 
-const newsIndices: string[] = ["0"];
+const interestsTopics = [
+  { num: "0", name: "정치", icon: Q10, eng: "Politics +" },
+  { num: "1", name: "생활 문화", icon: Q11, eng: "Culture +" },
+  { num: "2", name: "경제", icon: Q12, eng: "Economy +" },
+  { num: "3", name: "IT/과학", icon: Q13, eng: "IT/Science +" },
+  { num: "4", name: "사회", icon: Q14, eng: "Society +" },
+  { num: "5", name: "세계", icon: Q15, eng: "World +" },
+  { num: "6", name: "스포츠", icon: Q16, eng: "Sports +" },
+  { num: "7", name: "연예", icon: Q17, eng: "Entertainment +" },
+];
+
+const dummyUserData = {
+  name: "강해린",
+  id: "Babycattttt00",
+  userType: "자유로운 영혼의 도파민형",
+  assignDate: "2024-04-21",
+  interests: ["사회", "연예"],
+  keywords: ["교육", "건강정보", "뮤직"],
+  media: ["세계일보", "한겨레", "워싱턴포스트"],
+  profileImg: "프로필 이미지 url",
+  password: "existing password",
+};
+
+const dummyHeadlines = [
+  { articleId: 1, title: "예시 기사 제목1" },
+  { articleId: 2, title: "예시 기사 제목2" },
+  { articleId: 3, title: "예시 기사 제목3" },
+  { articleId: 4, title: "예시 기사 제목4" },
+  { articleId: 5, title: "예시 기사 제목5" },
+];
+
+const dummyMainNews = {
+  articleId: 1,
+  title: "예시 기사 제목",
+  previewImg:
+    "https://i.namu.wiki/i/U_Z88LvTS9NYuuKFRvLXJ0xa0h4h7sARaA-lP-FaPYWHophKDSUTGWtjGNtv198ixmw_SPx2MYb8EqN9upOm3rYdKXvdk24bP3FHi3TfAHdYLOdmpQGyYF5EYn4uYQFxC0n4BFKiB5dVzs0nqHlL3g.webp",
+  date: "2024-06-27",
+  scrap: false,
+};
 
 const MAIN_2: React.FC = () => {
+  const [userData, setUserData] = useState(dummyUserData); // 사용자 데이터 상태
+  const [filteredTopics, setFilteredTopics] = useState<any[]>([]); // 필터링된 토픽 상태
   const navigate = useNavigate();
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [isRedirectOpen, setIsRedirectOpen] = useState(false);
+
+  const handleHeadlineClick = (articleId: number) => {
+    navigate(`/articles/${articleId}`);
+  };
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function handleRedirectOpen() {
+    setIsOpen(false);
+    setIsRedirectOpen(true);
+  }
+
+  function handleRedirectClose() {
+    setIsRedirectOpen(false);
+  }
+
+  function handleSave(folderName: string) {
+    console.log("Folder saved:", folderName);
+  }
+
+  useEffect(() => {
+    setUserData(dummyUserData); // 실제로는 서버에서 데이터 가져오는 로직
+  }, []);
+
+  useEffect(() => {
+    if (userData.media) {
+      const filtered = topics.filter((topic) =>
+        userData.media.includes(topic.name)
+      );
+      setFilteredTopics(filtered);
+    }
+  }, [userData]);
 
   const handleButtonClick = (path: string) => {
     window.location.href = path;
+  };
+
+  const renderInterestTopics = () => {
+    return interestsTopics
+      .filter((topic) => userData.interests.includes(topic.name))
+      .map((topic) => (
+        <div className="main-table-cell-3" key={topic.num}>
+          <div className="main-table-text-container">
+            <img
+              src={topic.icon}
+              alt={topic.eng}
+              className="main-question-icon"
+            />
+            <p className="main-table-text">{topic.eng}</p>
+          </div>
+        </div>
+      ));
   };
 
   return (
@@ -93,29 +199,39 @@ const MAIN_2: React.FC = () => {
         <img src={TITLE} alt="TITLE" className="main-title-image" />
         <div className="main-content-container">
           <div className="main-left-box">
-            <p className="main-left-text-main">안녕하세요 기사 제목입니다.</p>
+            <p className="main-left-text-main">{dummyMainNews.title}</p>
             <p className="main-left-text">
-              2024.05.06
-              <img src={PENCIL} alt="Edit" className="main-pencil-icon-left" />
+              {dummyMainNews.date}
+              <img
+                src={PENCIL}
+                alt="Edit"
+                className="main-pencil-icon-left"
+                onClick={openModal}
+              />
             </p>
-            <img src={IMG} alt="Example" className="main-left-image" />
+            <img
+              src={dummyMainNews.previewImg}
+              alt="Example"
+              className="main-left-image"
+            />
           </div>
           <div className="main-right-box">
             <div className="main-header-buttons">
-              {newsIndices.length > 0 ? (
-                newsIndices.map((index) => {
-                  const iconIndex = parseInt(index, 10);
+              {filteredTopics.length > 0 ? (
+                filteredTopics.map((topic, index) => {
+                  const iconIndex = topics.findIndex(
+                    (t) => t.name === topic.name
+                  );
                   if (iconIndex >= 0 && iconIndex < newsIcons.length) {
-                    const topic = topics[iconIndex];
                     return (
                       <button
-                        key={index}
+                        key={topic.name}
                         className="main-news-button"
                         onClick={() => handleButtonClick(topic.path)}
                       >
                         <img
                           src={newsIcons[iconIndex]}
-                          alt={`News Icon ${index}`}
+                          alt={`News Icon ${iconIndex}`}
                           className="main-news-icon"
                         />
                       </button>
@@ -130,37 +246,20 @@ const MAIN_2: React.FC = () => {
               )}
             </div>
             <p className="main-HAD">How About This?</p>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
+            {dummyHeadlines.map((headline) => (
+              <button
+                key={headline.articleId}
+                className="main-headline-button"
+                onClick={() => handleHeadlineClick(headline.articleId)}
+              >
+                {headline.title}
+              </button>
+            ))}
           </div>
         </div>
         <div className="main-table-container">
           <div className="main-table-row main-table-header">
-            <div className="main-table-cell-3">
-              <div className="main-table-text-container">
-                <img src={Q10} alt="Politics" className="main-question-icon" />
-                <p className="main-table-text">Politics +</p>
-              </div>
-            </div>
-            <div className="main-table-cell-3">
-              <div className="main-table-text-container">
-                <img src={Q12} alt="Economy" className="main-question-icon" />
-                <p className="main-table-text">Economy +</p>
-              </div>
-            </div>
+            {renderInterestTopics()}
             <div className="main-table-cell-3">
               <div className="main-table-text-container">
                 <p className="main-table-text">+ 더보기</p>
@@ -178,7 +277,12 @@ const MAIN_2: React.FC = () => {
                 <p className="main-table-text">안녕하세요 기사 제목입니다.</p>
                 <p className="main-table-subtext">
                   2024.05.06
-                  <img src={PENCIL} alt="Edit" className="main-pencil-icon" />
+                  <img
+                    src={PENCIL}
+                    alt="Edit"
+                    className="main-pencil-icon"
+                    onClick={openModal}
+                  />
                 </p>
               </div>
               <div className="main-table-cell-3">
@@ -190,7 +294,12 @@ const MAIN_2: React.FC = () => {
                 <p className="main-table-text">안녕하세요 기사 제목입니다.</p>
                 <p className="main-table-subtext">
                   2024.05.06
-                  <img src={PENCIL} alt="Edit" className="main-pencil-icon" />
+                  <img
+                    src={PENCIL}
+                    alt="Edit"
+                    className="main-pencil-icon"
+                    onClick={openModal}
+                  />
                 </p>
               </div>
               <div className="main-table-cell-3">
@@ -207,6 +316,16 @@ const MAIN_2: React.FC = () => {
           ))}
         </div>
       </div>
+      <SaveScrap
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        onSave={handleSave}
+        onRedirectOpen={handleRedirectOpen}
+      />
+      <RedirectModal
+        isOpen={isRedirectOpen}
+        onRequestClose={handleRedirectClose}
+      />
     </div>
   );
 };
--- src/components/main/main_4.tsx
+++ src/components/main/main_4.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import WGO from "../../assets/icons/whats_going_on.svg";
 import TITLE from "../../assets/icons/main_title.svg";
 import IMG from "../../assets/icons/img.jpg";
@@ -8,6 +8,12 @@ import Q10 from "../../assets/icons/q_1_0.svg";
 import Q11 from "../../assets/icons/q_1_1.svg";
 import Q12 from "../../assets/icons/q_1_2.svg";
 import Q13 from "../../assets/icons/q_1_3.svg";
+import Q14 from "../../assets/icons/q_1_4.svg";
+import Q15 from "../../assets/icons/q_1_5.svg";
+import Q16 from "../../assets/icons/q_1_6.svg";
+import Q17 from "../../assets/icons/q_1_7.svg";
+import SaveScrap from "components/saveScrap";
+import RedirectModal from "components/redirectModal";
 import "./main.css";
 
 const newsIcons = [
@@ -66,12 +72,111 @@ const topics = [
   { name: "데일리안", path: "https://www.dailian.co.kr/" },
 ];
 
+const interestsTopics = [
+  { num: "0", name: "정치", icon: Q10, eng: "Politics +" },
+  { num: "1", name: "생활 문화", icon: Q11, eng: "Culture +" },
+  { num: "2", name: "경제", icon: Q12, eng: "Economy +" },
+  { num: "3", name: "IT/과학", icon: Q13, eng: "IT/Science +" },
+  { num: "4", name: "사회", icon: Q14, eng: "Society +" },
+  { num: "5", name: "세계", icon: Q15, eng: "World +" },
+  { num: "6", name: "스포츠", icon: Q16, eng: "Sports +" },
+  { num: "7", name: "연예", icon: Q17, eng: "Entertainment +" },
+];
+
+const dummyUserData = {
+  name: "강해린",
+  id: "Babycattttt00",
+  userType: "자유로운 영혼의 도파민형",
+  assignDate: "2024-04-21",
+  interests: ["사회", "연예", "세계", "정치"],
+  keywords: ["교육", "건강정보", "뮤직"],
+  media: ["세계일보", "한겨레", "전자신문"],
+  profileImg: "프로필 이미지 url",
+  password: "existing password",
+};
+
+const dummyHeadlines = [
+  { articleId: 1, title: "예시 기사 제목1" },
+  { articleId: 2, title: "예시 기사 제목2" },
+  { articleId: 3, title: "예시 기사 제목3" },
+  { articleId: 4, title: "예시 기사 제목4" },
+  { articleId: 5, title: "예시 기사 제목5" },
+];
+
+const dummyMainNews = {
+  articleId: 1,
+  title: "예시 기사 제목",
+  previewImg:
+    "https://i.namu.wiki/i/U_Z88LvTS9NYuuKFRvLXJ0xa0h4h7sARaA-lP-FaPYWHophKDSUTGWtjGNtv198ixmw_SPx2MYb8EqN9upOm3rYdKXvdk24bP3FHi3TfAHdYLOdmpQGyYF5EYn4uYQFxC0n4BFKiB5dVzs0nqHlL3g.webp",
+  date: "2024-06-27",
+  scrap: false,
+};
+
 const MAIN_4: React.FC = () => {
+  const [userData, setUserData] = useState(dummyUserData); // 사용자 데이터 상태
+  const [filteredTopics, setFilteredTopics] = useState<any[]>([]); // 필터링된 토픽 상태
   const navigate = useNavigate();
-  const newsIndices: string[] = ["2", "5", "23"];
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [isRedirectOpen, setIsRedirectOpen] = useState(false);
+
+  const handleHeadlineClick = (articleId: number) => {
+    navigate(`/articles/${articleId}`);
+  };
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function handleRedirectOpen() {
+    setIsOpen(false);
+    setIsRedirectOpen(true);
+  }
+
+  function handleRedirectClose() {
+    setIsRedirectOpen(false);
+  }
+
+  function handleSave(folderName: string) {
+    console.log("Folder saved:", folderName);
+  }
+
+  useEffect(() => {
+    setUserData(dummyUserData); // 실제로는 서버에서 데이터 가져오는 로직
+  }, []);
+
+  useEffect(() => {
+    if (userData.media) {
+      const filtered = topics.filter((topic) =>
+        userData.media.includes(topic.name)
+      );
+      setFilteredTopics(filtered);
+    }
+  }, [userData]);
 
   const handleButtonClick = (path: string) => {
     window.location.href = path;
+  };
+
+  const renderInterestTopics = () => {
+    return interestsTopics
+      .filter((topic) => userData.interests.includes(topic.name))
+      .map((topic) => (
+        <div className="main-table-cell-3" key={topic.num}>
+          <div className="main-table-text-container">
+            <img
+              src={topic.icon}
+              alt={topic.eng}
+              className="main-question-icon"
+            />
+            <p className="main-table-text">{topic.eng}</p>
+          </div>
+        </div>
+      ));
   };
 
   return (
@@ -94,29 +199,39 @@ const MAIN_4: React.FC = () => {
         <img src={TITLE} alt="TITLE" className="main-title-image" />
         <div className="main-content-container">
           <div className="main-left-box">
-            <p className="main-left-text-main">안녕하세요 기사 제목입니다.</p>
+            <p className="main-left-text-main">{dummyMainNews.title}</p>
             <p className="main-left-text">
-              2024.05.06
-              <img src={PENCIL} alt="Edit" className="main-pencil-icon-left" />
+              {dummyMainNews.date}
+              <img
+                src={PENCIL}
+                alt="Edit"
+                className="main-pencil-icon-left"
+                onClick={openModal}
+              />
             </p>
-            <img src={IMG} alt="Example" className="main-left-image" />
+            <img
+              src={dummyMainNews.previewImg}
+              alt="Example"
+              className="main-left-image"
+            />
           </div>
           <div className="main-right-box">
             <div className="main-header-buttons">
-              {newsIndices.length > 0 ? (
-                newsIndices.map((index) => {
-                  const iconIndex = parseInt(index, 10);
+              {filteredTopics.length > 0 ? (
+                filteredTopics.map((topic, index) => {
+                  const iconIndex = topics.findIndex(
+                    (t) => t.name === topic.name
+                  );
                   if (iconIndex >= 0 && iconIndex < newsIcons.length) {
-                    const topic = topics[iconIndex];
                     return (
                       <button
-                        key={index}
+                        key={topic.name}
                         className="main-news-button"
                         onClick={() => handleButtonClick(topic.path)}
                       >
                         <img
                           src={newsIcons[iconIndex]}
-                          alt={`News Icon ${index}`}
+                          alt={`News Icon ${iconIndex}`}
                           className="main-news-icon"
                         />
                       </button>
@@ -125,57 +240,26 @@ const MAIN_4: React.FC = () => {
                   return null;
                 })
               ) : (
-                <button className="main-disabled-button" disabled></button>
+                <button className="main-disabled-button" disabled>
+                  X
+                </button>
               )}
             </div>
             <p className="main-HAD">How About This?</p>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
-            <button className="main-headline-button">
-              데일리 헤드라인 추천 기사입니다.
-            </button>
+            {dummyHeadlines.map((headline) => (
+              <button
+                key={headline.articleId}
+                className="main-headline-button"
+                onClick={() => handleHeadlineClick(headline.articleId)}
+              >
+                {headline.title}
+              </button>
+            ))}
           </div>
         </div>
         <div className="main-table-container">
           <div className="main-table-row main-table-header">
-            <div className="main-table-cell">
-              <div className="main-table-text-container">
-                <img src={Q10} alt="Politics" className="main-question-icon" />
-                <p className="main-table-text">Politics +</p>
-              </div>
-            </div>
-            <div className="main-table-cell">
-              <div className="main-table-text-container">
-                <img src={Q12} alt="Economy" className="main-question-icon" />
-                <p className="main-table-text">Economy +</p>
-              </div>
-            </div>
-            <div className="main-table-cell">
-              <div className="main-table-text-container">
-                <img
-                  src={Q13}
-                  alt="IT/Science"
-                  className="main-question-icon"
-                />
-                <p className="main-table-text">IT/Science +</p>
-              </div>
-            </div>
-            <div className="main-table-cell">
-              <div className="main-table-text-container">
-                <img src={Q11} alt="Culture" className="main-question-icon" />
-                <p className="main-table-text">Culture +</p>
-              </div>
-            </div>
+            {renderInterestTopics()}
           </div>
           {[...Array(3)].map((_, index) => (
             <div className="main-table-row" key={index}>
@@ -188,7 +272,12 @@ const MAIN_4: React.FC = () => {
                 <p className="main-table-text">안녕하세요 기사 제목입니다.</p>
                 <p className="main-table-subtext">
                   2024.05.06
-                  <img src={PENCIL} alt="Edit" className="main-pencil-icon" />
+                  <img
+                    src={PENCIL}
+                    alt="Edit"
+                    className="main-pencil-icon"
+                    onClick={openModal}
+                  />
                 </p>
               </div>
               <div className="main-table-cell">
@@ -200,7 +289,12 @@ const MAIN_4: React.FC = () => {
                 <p className="main-table-text">안녕하세요 기사 제목입니다.</p>
                 <p className="main-table-subtext">
                   2024.05.06
-                  <img src={PENCIL} alt="Edit" className="main-pencil-icon" />
+                  <img
+                    src={PENCIL}
+                    alt="Edit"
+                    className="main-pencil-icon"
+                    onClick={openModal}
+                  />
                 </p>
               </div>
               <div className="main-table-cell">
@@ -212,7 +306,12 @@ const MAIN_4: React.FC = () => {
                 <p className="main-table-text">안녕하세요 기사 제목입니다.</p>
                 <p className="main-table-subtext">
                   2024.05.06
-                  <img src={PENCIL} alt="Edit" className="main-pencil-icon" />
+                  <img
+                    src={PENCIL}
+                    alt="Edit"
+                    className="main-pencil-icon"
+                    onClick={openModal}
+                  />
                 </p>
               </div>
               <div className="main-table-cell">
@@ -224,13 +323,28 @@ const MAIN_4: React.FC = () => {
                 <p className="main-table-text">안녕하세요 기사 제목입니다.</p>
                 <p className="main-table-subtext">
                   2024.05.06
-                  <img src={PENCIL} alt="Edit" className="main-pencil-icon" />
+                  <img
+                    src={PENCIL}
+                    alt="Edit"
+                    className="main-pencil-icon"
+                    onClick={openModal}
+                  />
                 </p>
               </div>
             </div>
           ))}
         </div>
       </div>
+      <SaveScrap
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        onSave={handleSave}
+        onRedirectOpen={handleRedirectOpen}
+      />
+      <RedirectModal
+        isOpen={isRedirectOpen}
+        onRequestClose={handleRedirectClose}
+      />
     </div>
   );
 };
